8aa54c6255accf1e6ee46dca4f589641c73fa04f
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2022-10-19 20:53:00 -0500

    ending discover page and adding play/pause functionality

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,8 +1,11 @@
+import { useDispatch, useSelector } from 'react-redux';
 import {Error, Loader, SongCard} from '../components';
 import {genres} from '../assets/constants'
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
+    const dispatch = useDispatch();
+    const { activeSong, isPlaying}= useSelector((state) => state.player);
     const {data, isFetching, error}= useGetTopChartsQuery();
     const genreTitle= 'Pop';
 
@@ -31,6 +34,9 @@ const Discover = () => {
                         key={song.key} 
                         song={song}
                         i={i}
+                        isPlaying={isPlaying}
+                        activeSong={activeSong}
+                        data={data}
                     />
                 ))}
             </div>
