3c749f929c028fda3dd544750b7a42305b5f2f02
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2022-10-28 15:31:20 -0500

    Adding Around You component, Related Songs and solving bugs

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -13,6 +13,9 @@ export const shazamCoreApi = createApi({
         getTopCharts: builder.query({query: ()=>'/charts/world'}),
         getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
         getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
+        getArtistDetails: builder.query({ query: ( artistId ) => `/artists/details?artist_id=${artistId}` }),
+        getSongsByCountry: builder.query({ query: ( country ) => `/charts/country?country=${country}` }),
+        
 
     }),
 });
@@ -20,4 +23,6 @@ export const {
     useGetTopChartsQuery,
     useGetSongDetailsQuery,
     useGetSongRelatedQuery,
+    useGetArtistDetailsQuery,
+    useGetSongsByCountryQuery,
 }= shazamCoreApi
